752b63e55811e2c24ab807251a3230b9edd2228a
Haneul Lee <lovesky4294@gmail.com>
2019-03-09 20:30:31 +0900

    react practice first commit - props, state - style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CountryList from "./CountryList";
+import Footer from "./Footer";
+import styles from "./styles";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: "hello",
+      list: [
+        { no: 1, country: "egypt", visited: false },
+        { no: 2, country: "turkey", visited: true },
+        { no: 3, country: "korea", visited: true }
+      ]
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <h1>{this.state.msg}</h1>
+        <hr style={styles}/>
+        <CountryList countries={this.state.list} /><Footer/>
       </div>
     );
   }
---
+++ src/CountryItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import styles from "./styles";
+
+class CountryItem extends Component {
+  render() {
+    return (
+      <li style={styles.listItemStyle}
+        className={
+          this.props.visited ? "list-group-item active" : "list-groupt-item"
+        }
+      >
+        {this.props.country}
+      </li>
+    );
+  }
+}
+
+export default CountryItem;
